5e352196486f3d47ec1264177cef3bad4ac23981
AngelManchevC4 <angel.manchev@c4nexus.com>
2023-10-24 20:13:36 +0300

    Account controller extended additional logic added (need to add more logic here to make it work), profile form extended additional fields added everything done , template extended added additional html fields and  render logic done template done, forms.properties extended additional properties added

---
+++ cartridges/app_storefront_custom2/cartridge/controllers/Account.js
@@ -0,0 +1,172 @@
+var server = require('server');
+
+var csrfProtection = require('*/cartridge/scripts/middleware/csrf');
+var userLoggedIn = require('*/cartridge/scripts/middleware/userLoggedIn');
+var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
+
+
+var base = server.extend(module.superModule);
+
+server.replace(
+    'EditProfile',
+    server.middleware.https,
+    csrfProtection.generateToken,
+    userLoggedIn.validateLoggedIn,
+    consentTracking.consent,
+    function (req, res, next) {
+        var ContentMgr = require('dw/content/ContentMgr');
+        var Resource = require('dw/web/Resource');
+        var URLUtils = require('dw/web/URLUtils');
+        var accountHelpers = require('*/cartridge/scripts/account/accountHelpers');
+
+        var accountModel = accountHelpers.getAccountModel(req);
+        var content = ContentMgr.getContent('tracking_hint');
+        var profileForm = server.forms.getForm('profile');
+        profileForm.clear();
+        profileForm.customer.firstname.value = accountModel.profile.firstName;
+        profileForm.customer.lastname.value = accountModel.profile.lastName;
+        profileForm.customer.phone.value = accountModel.profile.phone;
+        profileForm.customer.email.value = accountModel.profile.email;
+        profileForm.customer.fieldinterests.value = accountModel.profile.fieldInterests;
+        profileForm.customer.countryofresidence.value = accountModel.profile.countryOfResidence;
+
+        // DISCUSS ON DAILY
+        var TEST = accountModel.profile;
+
+        res.render('account/profile', {
+            consentApi: Object.prototype.hasOwnProperty.call(req.session.raw, 'setTrackingAllowed'),
+            caOnline: content ? content.online : false,
+            profileForm: profileForm,
+            breadcrumbs: [
+                {
+                    htmlValue: Resource.msg('global.home', 'common', null),
+                    url: URLUtils.home().toString()
+                },
+                {
+                    htmlValue: Resource.msg('page.title.myaccount', 'account', null),
+                    url: URLUtils.url('Account-Show').toString()
+                }
+            ]
+        });
+        next();
+    }
+);
+
+server.replace(
+    'SaveProfile',
+    server.middleware.https,
+    csrfProtection.validateAjaxRequest,
+    function (req, res, next) {
+        var Transaction = require('dw/system/Transaction');
+        var CustomerMgr = require('dw/customer/CustomerMgr');
+        var Resource = require('dw/web/Resource');
+        var URLUtils = require('dw/web/URLUtils');
+        var accountHelpers = require('*/cartridge/scripts/helpers/accountHelpers');
+
+        var formErrors = require('*/cartridge/scripts/formErrors');
+
+        var profileForm = server.forms.getForm('profile');
+
+        // form validation
+        if (profileForm.customer.email.value.toLowerCase()
+            !== profileForm.customer.emailconfirm.value.toLowerCase()) {
+            profileForm.valid = false;
+            profileForm.customer.email.valid = false;
+            profileForm.customer.emailconfirm.valid = false;
+            profileForm.customer.emailconfirm.error =
+                Resource.msg('error.message.mismatch.email', 'forms', null);
+        }
+
+        var result = {
+            firstName: profileForm.customer.firstname.value,
+            lastName: profileForm.customer.lastname.value,
+            phone: profileForm.customer.phone.value,
+            email: profileForm.customer.email.value,
+            confirmEmail: profileForm.customer.emailconfirm.value,
+            password: profileForm.login.password.value,
+            countryOfResidence:profileForm.customer.countryofresidence.value,
+            fieldInterests:profileForm.customer.fieldinterests.value,
+            profileForm: profileForm
+        };
+        if (profileForm.valid) {
+            res.setViewData(result);
+            this.on('route:BeforeComplete', function (req, res) { // eslint-disable-line no-shadow
+                var formInfo = res.getViewData();
+                var customer = CustomerMgr.getCustomerByCustomerNumber(
+                    req.currentCustomer.profile.customerNo
+                );
+                var profile = customer.getProfile();
+                var customerLogin;
+                var status;
+
+                Transaction.wrap(function () {
+                    status = profile.credentials.setPassword(
+                        formInfo.password,
+                        formInfo.password,
+                        true
+                    );
+
+                    if (status.error) {
+                        formInfo.profileForm.login.password.valid = false;
+                        formInfo.profileForm.login.password.error =
+                            Resource.msg('error.message.currentpasswordnomatch', 'forms', null);
+                    } else {
+                        customerLogin = profile.credentials.setLogin(
+                            formInfo.email,
+                            formInfo.password
+                        );
+                    }
+                });
+
+                delete formInfo.password;
+                delete formInfo.confirmEmail;
+
+                if (customerLogin) {
+                    Transaction.wrap(function () {
+                        profile.setFirstName(formInfo.firstName);
+                        profile.setLastName(formInfo.lastName);
+                        profile.setEmail(formInfo.email);
+                        profile.setPhoneHome(formInfo.phone);
+                        profile.getCustom().fieldInterests = profileForm.customer.fieldinterests.value;
+                        profile.getCustom().countryOfResidence = profileForm.customer.countryofresidence.value;
+                    });
+
+                    // Send account edited email
+                    accountHelpers.sendAccountEditedEmail(customer.profile);
+
+                    delete formInfo.profileForm;
+                    delete formInfo.email;
+
+                    res.json({
+                        success: true,
+                        redirectUrl: URLUtils.url('Account-Show').toString()
+                    });
+                } else {
+                    if (!status.error) {
+                        formInfo.profileForm.customer.email.valid = false;
+                        formInfo.profileForm.customer.email.error =
+                            Resource.msg('error.message.username.invalid', 'forms', null);
+                    }
+
+                    delete formInfo.profileForm;
+                    delete formInfo.email;
+
+                    res.json({
+                        success: false,
+                        fields: formErrors.getFormErrors(profileForm)
+                    });
+                }
+            });
+        } else {
+            res.json({
+                success: false,
+                fields: formErrors.getFormErrors(profileForm)
+            });
+        }
+        return next();
+    }
+);
+
+
+
+module.exports = server.exports();

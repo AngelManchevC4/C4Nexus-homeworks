fc4e8a6b2bd97bfc980128a19aa81c07cf9730f9
AngelManchevC4 <angel.manchev@c4nexus.com>
2023-10-26 13:59:55 +0300

    code refactor

--- cartridges/app_storefront_custom2/cartridge/models/address.js
+++ cartridges/app_storefront_custom2/cartridge/models/address.js
@@ -1,9 +1,9 @@
 var base = module.superModule;
 
-function address(addressObject){
-    base.call(this,address);
-    
-    this.workAddress =addressObject.workAddress
+function address(addressObject) {
+    base.call(this, address);
+
+    this.workAddress = addressObject.workAddress
 }
 
 module.exports = address;

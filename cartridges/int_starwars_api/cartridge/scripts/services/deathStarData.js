a9257e8cb12406ddbbd05b7a3e99c3dfb8becef3
AngelManchevC4 <angel.manchev@c4nexus.com>
2023-10-31 17:42:53 +0200

    code fix

--- cartridges/int_starwars_api/cartridge/scripts/services/deathStarData.js
+++ cartridges/int_starwars_api/cartridge/scripts/services/deathStarData.js
@@ -9,7 +9,7 @@ function getDeathStarData() {
         parseResponse: function (svc, client) {
             return client.text
         },
-        filterLogMessage(msg) {
+        filterLogMessage: function(msg) {
             return msg.replace(/cost_in_credits\: \".*?\"/, "cost_in_credits:$$$$$$$$$$$$$$$$$$$");
         }
     })
